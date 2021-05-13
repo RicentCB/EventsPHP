1fa47efcdd33afb96c940530b551ef55ff01ea65
RicentCB <ricardo.cb009@gmail.com>
2021-05-13 15:31:03 -0500

    [ADD] Eventos registrados y no registrados

--- ajax/events.ajax.php
+++ ajax/events.ajax.php
@@ -1,14 +1,17 @@
 <?php
     // Controlador
     require "../controller/events.controller.php";
+    require "../controller/eventsUsers.controller.php";
     //Modelo
     require "../model/events.model.php";
+    require "../model/eventsUsers.model.php";
 
     if(isset($_GET["action"])){
         switch($_GET["action"]){
             case 'readEvents':
-                $users = EventsController::ctrReadEvents();
-                echo json_encode(array("type"=>"success", "message"=>$users));
+                $idUser = $_GET["idUser"];
+                $eventsAvaileble = EventsController::ctrGetNotRegisteredEvents($idUser);
+                echo json_encode(array("type"=>"success", "message"=>$eventsAvaileble));
                 break;
             default:
                 echo json_encode(array("type"=>"error", "message"=>"Accion no coincide con niguna opcion disponible"));
--- controller/events.controller.php
+++ controller/events.controller.php
@@ -5,4 +5,44 @@
             $events = EventsModel::mdlReadEvents($item, $value);
             return $events;
         }
+        // Obtener eventos a los cuales no este registrado el usuario
+        static public function ctrGetNotRegisteredEvents($idUser){
+            //Leer todos los eventos
+            $allEvents = EventsController::ctrReadEvents();
+            //Leer eventos a los cuales ya se ha registrado el usuario
+            $eventsRegistered = EventsUsersController::ctrReadEventsUsers("usuarioID", $idUser);
+            $eventsNotRegistered = array();
+            //Verficiar que eventos no se ha inscrito el usuario
+            foreach ($allEvents as $event) {
+                $isAlreadyRegistered = false;
+                foreach($eventsRegistered as $eventRegistered){
+                    if($event["eventoID"] == $eventRegistered["eventoID"])
+                        $isAlreadyRegistered = true;
+                }
+                if(!$isAlreadyRegistered)
+                    array_push($eventsNotRegistered, $event);
+            }
+            return $eventsNotRegistered;
+        }
+
+
+        // Obtener eventos a los cuales YA este registrado el usuario
+        static public function ctrGetRegisteredEvents($idUser){
+            //Leer todos los eventos
+            $allEvents = EventsController::ctrReadEvents();
+            //Leer eventos a los cuales ya se ha registrado el usuario
+            $eventsRegistered = EventsUsersController::ctrReadEventsUsers("usuarioID", $idUser);
+            $eventsRegistered = array();
+            //Verficiar que eventos no se ha inscrito el usuario
+            foreach ($allEvents as $event) {
+                $isAlreadyRegistered = false;
+                foreach($eventsRegistered as $eventRegistered){
+                    if($event["eventoID"] == $eventRegistered["eventoID"])
+                        $isAlreadyRegistered = true;
+                }
+                if($isAlreadyRegistered)
+                    array_push($eventsRegistered, $event);
+            }
+            return $eventsRegistered;
+        }
     }
