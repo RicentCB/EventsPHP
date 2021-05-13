7f1c2f7f5389a283d3679f30555962c51cb68d30
RicentCB <ricardo.cb009@gmail.com>
2021-05-13 15:41:18 -0500

    [ADD] Registrar usuario en el evento

--- model/eventsUsers.model.php
+++ model/eventsUsers.model.php
@@ -2,7 +2,25 @@
     require_once ("conection.php");
 
     class EventsUsersModel{
-        /* Leer Datos de usuario */
+        /* Leer Datos de los eventos por usuario */
+        static public function mdlTest(){
+            $sql  = "DESCRIBE eventos_usuarios ";
+            
+            $stmt = Conexion::connect() -> prepare($sql);
+            $stmt -> execute();
+            return $stmt -> fetchALl();
+
+        }
+        static public function mdlCleanEventsUsers(){
+            $sql  = "DELETE FROM eventos_usuarios ";
+            
+            $stmt = Conexion::connect() -> prepare($sql);
+            if($stmt -> execute())
+                return true;
+            else
+                return false;
+        }
+        /* Leer Datos de los eventos por usuario */
         static public function mdlReadEventsUsers($item, $value){
             $sql  = "SELECT * FROM eventos_usuarios ";
             
@@ -15,7 +33,29 @@
                 $stmt = Conexion::connect() -> prepare($sql);
                 $stmt -> bindParam(":item", $value);
                 $stmt -> execute();
-                return $stmt -> fetch();
+                return $stmt -> fetchAll();
             }
+        }
+        // Crear un evento por usuario
+        static public function mdlCreateEventUsers($data){
+            $idUser = $data["idUser"];
+            $idEvent = $data["idEvent"];
+            
+            $sql = "INSERT INTO `eventos_usuarios`(`eventoID`, `usuarioID`) ";            
+            $sql .= "VALUES (:idEvent, :idUser)";
+
+            $pdo = Conexion::connect();
+            $stmt = $pdo -> prepare($sql);
+
+            $stmt -> bindParam(":idEvent", $idEvent);
+            $stmt -> bindParam(":idUser", $idUser);
+
+            if($stmt->execute())
+                return true;
+            else
+                return false;
+            $stmt -> close();
+            $stmt = null;
+
         }
     }
